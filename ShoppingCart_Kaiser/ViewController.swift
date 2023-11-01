e20a38b3dc2fcd2655ac1dadc8f653963761f8d1
Andrew Kaiser <Andrewkaiser2006@gmail.com>
2023-11-01 09:50:21 -0500

    have checkmarks present

--- ShoppingCart_Kaiser/ViewController.swift
+++ ShoppingCart_Kaiser/ViewController.swift
@@ -17,42 +17,86 @@ class ViewController: UIViewController, UITableViewDelegate ,UITableViewDataSour
     @IBOutlet weak var tableViewOutlet: UITableView!
     @IBOutlet weak var addFieldOutlet: UITextField!
     
-    var defaults = UserDefaults.standard
     
-    var items = [String]()
+    var defaults = UserDefaults.standard
+    var cart = [String]()
+    //alert controllers
+    var alert: UIAlertController!
+    var okAction: UIAlertAction!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableViewOutlet.dataSource = self
         tableViewOutlet.delegate = self
         // Do any additional setup after loading the view.
-        items.append("Chicken")
+        cart.append("Chicken")
+        
+        //alerts
+        alert = UIAlertController(title: "Overlapping Items", message: "Item is already in cart", preferredStyle: .alert)
+        okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        //
+        if let items = defaults.data(forKey: "theCart") {
+                        let decoder = JSONDecoder()
+                        if let decoded = try? decoder.decode([String].self, from: items) {
+                            cart = decoded
+                        }
+                }
+        tableViewOutlet.reloadData()
     }
     
     
     @IBAction func addButton(_ sender: Any) {
-        items.append(addFieldOutlet.text!)
+        for x in cart{
+            if x == addFieldOutlet.text {
+                present(alert, animated: true)
+                addFieldOutlet.text = ""
+                return
+            }
+        }
+        cart.append(addFieldOutlet.text!)
         tableViewOutlet.reloadData()
     }
     
     @IBAction func saveAction(_ sender: Any) {
         let encoder = JSONEncoder()
-           if let encoded = try? encoder.encode(items) {
-                            UserDefaults.standard.set(encoded, forKey: "theCart")
+           if let encoded = try? encoder.encode(cart) {
+                            defaults.set(encoded, forKey: "theCart")
                         }
     }
+    
+    @IBAction func sortAction(_ sender: Any) {
+        cart.sort(by: <)
+        tableViewOutlet.reloadData()
+    }
+    
     
     
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return cart.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
-        cell.textLabel?.text = "\(items[indexPath.row])"
+        cell.accessoryType = .checkmark
+        cell.textLabel?.text = "\(cart[indexPath.row])"
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+//       // let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+//        .accessoryType = .checkmark
+//        tableView.reloadData()
+    }
+    //deleting if user swipes left
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            cart.remove(at: indexPath.row)
+            tableViewOutlet.deleteRows(at: [indexPath], with: .fade)
+            tableViewOutlet.reloadData()
+        }
     }
 
 
